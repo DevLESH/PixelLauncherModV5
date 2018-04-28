debd289b2517d1dd65a0bfc25bfeafe988d15411
paphonb <paphonb@gmail.com>
2018-04-29 00:21:44 +0700

    Fix build script

--- ci.sh
+++ ci.sh
@@ -5,9 +5,9 @@ echo 'Building normal variant...'
 ./tools/yq_linux_386 w -i apktool.yml versionInfo.versionName "P-4623511 modded 5.3 build ${TRAVIS_BUILD_NUMBER}"
 ./build.sh
 echo 'Building Pixel variant...'
-sed -i '' 's/com.google.android.apps.nexuslauncher.permission./com.google.android.apps.moddednexuslauncher.permission./g' AndroidManifest.xml
-sed -i '' 's/com.google.android.apps.nexuslauncher.settings/com.google.android.apps.moddednexuslauncher.settings/g' AndroidManifest.xml
-sed -i '' 's/com.google.android.apps.nexuslauncher.appssearch/com.google.android.apps.moddednexuslauncher.appssearch/g' AndroidManifest.xml
+sed -i -e 's/com.google.android.apps.nexuslauncher.permission./com.google.android.apps.moddednexuslauncher.permission./g' AndroidManifest.xml
+sed -i -e 's/com.google.android.apps.nexuslauncher.settings/com.google.android.apps.moddednexuslauncher.settings/g' AndroidManifest.xml
+sed -i -e 's/com.google.android.apps.nexuslauncher.appssearch/com.google.android.apps.moddednexuslauncher.appssearch/g' AndroidManifest.xml
 ./tools/yq_linux_386 w -i apktool.yml apkFileName "PixelLauncherModV5-Pixel.apk"
 ./tools/yq_linux_386 w -i apktool.yml versionInfo.versionName "P-4623511 modded 5.3 build ${TRAVIS_BUILD_NUMBER} for Pixel"
 ./build.sh
