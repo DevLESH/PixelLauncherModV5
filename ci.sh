e917c6d38412c177133ed74a1cc4aa150c1d9be7
paphonb <paphonb@gmail.com>
2018-04-29 01:36:04 +0700

    Fix package name for the new variant

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ echo 'Building normal variant...'
 ./tools/yq_linux_386 w -i apktool.yml versionInfo.versionName "P-4623511 modded 5.3 build ${TRAVIS_BUILD_NUMBER}"
 ./build.sh
 echo 'Building Pixel variant...'
+sed -i -e '0,/com.google.android.apps.nexuslauncher/s//com.google.android.apps.moddednexuslauncher/' AndroidManifest.xml
 sed -i -e 's/com.google.android.apps.nexuslauncher.permission./com.google.android.apps.moddednexuslauncher.permission./g' AndroidManifest.xml
 sed -i -e 's/com.google.android.apps.nexuslauncher.settings/com.google.android.apps.moddednexuslauncher.settings/g' AndroidManifest.xml
 sed -i -e 's/com.google.android.apps.nexuslauncher.appssearch/com.google.android.apps.moddednexuslauncher.appssearch/g' AndroidManifest.xml
