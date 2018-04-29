f0bc55de0b297f8ad82157441e075a0c292443f5
paphonb <paphonb@gmail.com>
2018-04-29 09:06:52 +0700

    Fix crash for alternate variant

--- ci.sh
+++ ci.sh
@@ -9,6 +9,10 @@ sed -i -e '0,/com.google.android.apps.nexuslauncher/s//com.google.android.apps.m
 sed -i -e 's/com.google.android.apps.nexuslauncher.permission./com.google.android.apps.moddednexuslauncher.permission./g' AndroidManifest.xml
 sed -i -e 's/com.google.android.apps.nexuslauncher.settings/com.google.android.apps.moddednexuslauncher.settings/g' AndroidManifest.xml
 sed -i -e 's/com.google.android.apps.nexuslauncher.appssearch/com.google.android.apps.moddednexuslauncher.appssearch/g' AndroidManifest.xml
+sed -i -e 's/com.google.android.apps.nexuslauncher.appssearch/com.google.android.apps.moddednexuslauncher.appssearch/g' smali/com/google/android/apps/nexuslauncher/search/AppSearchProvider.smali
+sed -i -e 's/com.google.android.apps.nexuslauncher.appssearch/com.google.android.apps.moddednexuslauncher.appssearch/g' smali/com/google/android/apps/nexuslauncher/search/e.smali
+sed -i -e 's/com.google.android.apps.nexuslauncher/com.google.android.apps.moddednexuslauncher/g' smali/com/android/launcher3/config/FeatureFlags.smali
+sed -i -e '0,/com.google.android.apps.nexuslauncher/s//com.google.android.apps.moddednexuslauncher/g' res/xml/launcher_preferences.xml
 ./tools/yq_linux_386 w -i apktool.yml apkFileName "PixelLauncherModV5-Pixel.apk"
 ./tools/yq_linux_386 w -i apktool.yml versionInfo.versionName "P-4623511 modded 5.3 build ${TRAVIS_BUILD_NUMBER} for Pixel"
 ./build.sh
