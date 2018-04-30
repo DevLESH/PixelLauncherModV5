6be9b6c31777c6f67937d9630589ff528d1231c2
paphonb <paphonb@gmail.com>
2018-04-30 23:28:11 +0700

    Clean up build output before building Pixel variant

--- ci.sh
+++ ci.sh
@@ -4,6 +4,8 @@ echo 'Generating changelog...'
 echo 'Building normal variant...'
 ./tools/yq_linux_386 w -i apktool.yml versionInfo.versionName "P-4623511 modded 5.3 build ${TRAVIS_BUILD_NUMBER}"
 ./build.sh
+echo 'Cleaning up...'
+rm -rf build
 echo 'Building Pixel variant...'
 sed -i -e '0,/com.google.android.apps.nexuslauncher/s//com.google.android.apps.moddednexuslauncher/' AndroidManifest.xml
 sed -i -e 's/com.google.android.apps.nexuslauncher.permission./com.google.android.apps.moddednexuslauncher.permission./g' AndroidManifest.xml
